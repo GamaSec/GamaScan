cfa935de0f963545db24fa38bd4637c2dacdbf4a
boaz <g.boaz@gamasec.com>
2013-01-07 14:41:59 +0200

    Update public/javascripts/gamasec_cf.js

--- public/javascripts/gamasec_cf.js
+++ public/javascripts/gamasec_cf.js
@@ -2,10 +2,10 @@ CloudFlare.define("GamaSec",
     [       "GamaSec/config", "cloudflare/jquery1.7"],
     function(_config,$)
     {		
-		function addBadge(){
+		function addBadge(logoside){
 			var protocol = window.location.protocol != "https:" ? "http:" : "https:";
 			//var embeddedCode = "<a href=\""+protocol+"//www.iubenda.com/privacy-policy/1/"+_config.domain_id+"\" class=\"iubenda-white iubenda-embed no-brand iub-anchor\" title=\"Privacy Policy\">Privacy Policy</a><script type=\"text/javascript\">(function (w,d) {var loader = function () {var s = d.createElement(\"script\"), tag = d.getElementsByTagName(\"script\")[0]; s.src = \""+protocol+"//cdn.iubenda.com/iubenda.js\"; tag.parentNode.insertBefore(s,tag);}; loader();})(window, document);</script>";
-			var embeddedCode ="<div id=\"GamaSec-badge\" style=\"bottom:0;position:absolute;right:0;\"><a href=\""+protocol+"://www.gamasec.com/Seal.aspx?domain="+_config.domain_id+"\" target=\"_blank\" onclick=\"javascript:void window.open('"+protocol+"://www.gamasec.com/Seal.aspx?domain="+_config.domain_id+"','GamaScan','toolbar=0,resizable=0,width=532px,height=700px,location=0,menubar=0,scrollbars=0,status=0','');return false;\"> <table border=\"0\" width=\"121\" cellspacing=\"0\" cellpadding=\"0\"><tr><td><img height=\"53\" src=\""+protocol+"://www.gamasec.com/images/seals/tseal.png\" width=\"117\" border=\"0\"></td></tr><tr><td><p align=\"center\" alt=\"GamaScan\" oncontextmenu=\"alert('Copyright 2013 Gamasec Ltd | All rights reserved.');return false;\"><img border=\"0\" src=\""+protocol+"://www.gamasec.com/images/seals/DateBox.png\" width=\"118\" height=\"14\"></td></tr></table></a></div>";
+			var embeddedCode ="<div id=\"GamaSec-badge\" style=\"bottom:0;position:absolute;"+logoside+":0;\"><a href=\""+protocol+"://www.gamasec.com/Seal.aspx?domain="+_config.domain_id+"\" target=\"_blank\" onclick=\"javascript:void window.open('"+protocol+"://www.gamasec.com/Seal.aspx?domain="+_config.domain_id+"','GamaScan','toolbar=0,resizable=0,width=532px,height=700px,location=0,menubar=0,scrollbars=0,status=0','');return false;\"> <table border=\"0\" width=\"121\" cellspacing=\"0\" cellpadding=\"0\"><tr><td><img height=\"53\" src=\""+protocol+"://www.gamasec.com/images/seals/tseal.png\" width=\"117\" border=\"0\"></td></tr><tr><td><p align=\"center\" alt=\"GamaScan\" oncontextmenu=\"alert('Copyright 2013 Gamasec Ltd | All rights reserved.');return false;\"><img border=\"0\" src=\""+protocol+"://www.gamasec.com/images/seals/DateBox.png\" width=\"118\" height=\"14\"></td></tr></table></a></div>";
 			$("body").append(embeddedCode);
 		}
 		function lookForBadge(){
@@ -16,7 +16,7 @@ CloudFlare.define("GamaSec",
 			{
 				if(!$("GamaSec-badge")[0] &&!$(".GamaSec-badge")[0] && !$(".GamaSec-embed")[0] && !$("#GamaSec-embed")[0])
 				{
-					addBadge();
+					addBadge(_config.seal_badge);
 				}
 				else
 				{
