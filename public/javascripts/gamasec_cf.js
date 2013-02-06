c7471d15c6d1c91045f30e5cb45a800591a22e5c
boaz <g.boaz@gamasec.com>
2013-02-06 19:24:31 +0200

    Update public/javascripts/gamasec_cf.js

--- public/javascripts/gamasec_cf.js
+++ public/javascripts/gamasec_cf.js
@@ -1,21 +1,21 @@
-CloudFlare.define("GamaSec",
-    ["GamaSec/config", "cloudflare/jquery1.7"],
-    function(_config,$)
+CloudFlare.define("gamasec",
+    ["gamasec/config", "cloudflare/jquery1.7"],
+    function(config,$)
     {		
 		function addBadge(logoside){
 			var protocol = window.location.protocol != "https:" ? "http:" : "https:";
-			var embeddedCode ="<div id=\"GamaSec-badge\" style=\"bottom:0;position:absolute;"+logoside+":0;\"><a href=\""+protocol+"://www.gamasec.com/Seal.aspx?domain="+_config.domain_id+"\" target=\"_blank\" onclick=\"javascript:void window.open('"+protocol+"://www.gamasec.com/Seal.aspx?domain="+_config.domain_id+"','GamaScan','toolbar=0,resizable=0,width=532px,height=700px,location=0,menubar=0,scrollbars=0,status=0','');return false;\"> <table border=\"0\" width=\"121\" cellspacing=\"0\" cellpadding=\"0\"><tr><td><img height=\"53\" src=\""+protocol+"://www.gamasec.com/images/seals/tseal.png\" width=\"117\" border=\"0\"></td></tr><tr><td><p align=\"center\" alt=\"GamaScan\" oncontextmenu=\"alert('Copyright 2013 Gamasec Ltd | All rights reserved.');return false;\"><img border=\"0\" src=\""+protocol+"://www.gamasec.com/images/seals/DateBox.png\" width=\"118\" height=\"14\"></td></tr></table></a></div>";
+			var embeddedCode ="<div id=\"GamaSec_badge\" style=\"bottom:0;position:absolute;"+logoside+":0;\"><a href=\""+protocol+"://www.gamasec.com/Seal.aspx?domain="+config.domain_id+"\" target=\"_blank\" onclick=\"javascript:void window.open('"+protocol+"://www.gamasec.com/Seal.aspx?domain="+config.domain_id+"','GamaScan','toolbar=0,resizable=0,width=532px,height=700px,location=0,menubar=0,scrollbars=0,status=0','');return false;\"> <table border=\"0\" width=\"121\" cellspacing=\"0\" cellpadding=\"0\"><tr><td><img height=\"53\" src=\""+protocol+"://www.gamasec.com/images/seals/tseal.png\" width=\"117\" border=\"0\"></td></tr><tr><td><p align=\"center\" alt=\"GamaScan\" oncontextmenu=\"alert('Copyright 2013 Gamasec Ltd | All rights reserved.');return false;\"><img border=\"0\" src=\""+protocol+"://www.gamasec.com/images/seals/DateBox.png\" width=\"118\" height=\"14\"></td></tr></table></a></div>";
 			$("body").append(embeddedCode);
 		}
 		function lookForBadge(){
-			if(_config.seal_badge!="None")
+			if(config.seal_badge!="None")
 			{
-				if(!$("GamaSec-badge")[0])
+				if(!$("GamaSec_badge")[0])
 				{
-					addBadge(_config.seal_badge);
+					addBadge(config.seal_badge);
 				}
 			}
 		}
 		lookForBadge();
     }
-)
+);
