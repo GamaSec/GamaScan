3cdd0923733715b5731c95b6da9ff83f5a9bb83d
boaz <g.boaz@gamasec.com>
2014-01-23 14:13:11 +0200

    Update gamasec_cf.js

--- public/javascripts/gamasec_cf.js
+++ public/javascripts/gamasec_cf.js
@@ -3,7 +3,13 @@ CloudFlare.define("gamasec",
     function(config,$)
     {		
 		function addBadge(logoside){
-			var embeddedCode ="<div id=\"gamasec_badge\" style=\"bottom:0;position:fixed;"+logoside+":0;\"><a href=\"https://www.gamasec.com/Seal.aspx?domain="+config.domain_id+"\" target=\"_blank\" onClick=\"javascript:void window.open('https://www.gamasec.com/Seal.aspx?domain="+config.domain_id+"','GamaScan','toolbar=0,resizable=0,width=532px,height=700px,location=0,menubar=0,scrollbars=0,status=0','');return false;\"> <img border=\"0\" src=\"https://www.gamasec.com/images/seals/wseal.png\" alt=\"GamaScan\" oncontextmenu=\"alert('Copyright 2013 Gamasec Ltd | All rights reserved.');return false;\"></a></div>";
+			var domainurl=config.domain_id;
+			
+			if( domainurl  == 0 && domainurl ==undefined)
+			    {
+			    domainurl=location.hostname;
+			    }
+			var embeddedCode ="<div id=\"gamasec_badge\" style=\"bottom:0;position:fixed;"+logoside+":0;\"><a href=\"https://www.gamasec.com/Seal.aspx?domain="+domainurl+"\" target=\"_blank\" onClick=\"javascript:void window.open('https://www.gamasec.com/Seal.aspx?domain="+domainurl+"','GamaScan','toolbar=0,resizable=0,width=532px,height=700px,location=0,menubar=0,scrollbars=0,status=0','');return false;\"> <img border=\"0\" src=\"https://www.gamasec.com/images/seals/wseal.png\" alt=\"GamaScan\" oncontextmenu=\"alert('Copyright 2013 Gamasec Ltd | All rights reserved.');return false;\"></a></div>";
 			$("body").append(embeddedCode);
 		}
 		function lookForBadge(){
